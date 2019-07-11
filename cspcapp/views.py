5c4a6ddea89846605059ea354afc171807646f8c
nikhovas <nikhovas@ya.ru>
2019-07-11 18:59:23 +0500

    add login require to all pages

--- cspcapp/views.py
+++ cspcapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from .models import *
 from django.core.handlers.wsgi import WSGIRequest
 from .utilities import reformat_request_get_params, overview_get_format
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def students_overview(request: WSGIRequest):
     request_get = dict(overview_get_format(reformat_request_get_params(request.GET)))
     query = StudentsOverviewFunction.execute(**request_get)
@@ -11,13 +13,12 @@ def students_overview(request: WSGIRequest):
     return render(request, 'students_overview.html', {'students_list': query, 'request_get': get_request_preview})
 
 
+@login_required
 def student_detail_view(request: WSGIRequest, pk: int):
     student_person = StudentPerson.objects.filter(pk=pk)[0]
     student_info = {'student_person': student_person,
                     'person': student_person.person,
-                    'contracts': Contract.objects.filter(student_document__person=pk),
-                    }
-
+                    'contracts': Contract.objects.filter(student_document__person=pk)}
     return render(request, 'student_detail_view.html', student_info)
 
 
